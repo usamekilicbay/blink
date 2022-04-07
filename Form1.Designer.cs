8e0c897d9595047792a237d1fa47efa1c076ff84
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-04-07 13:15:12 +0300

    refactor: finish project add all features to From1.cs and Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,99 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.lbl_command = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl_timer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_timer.Font = new System.Drawing.Font("Montserrat Black", 102F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_timer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(214)))), ((int)(((byte)(252)))));
+            this.lbl_timer.Location = new System.Drawing.Point(366, 100);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(500, 300);
+            this.lbl_timer.TabIndex = 0;
+            this.lbl_timer.Text = "59";
+            this.lbl_timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_command
+            // 
+            this.lbl_command.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl_command.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbl_command.Font = new System.Drawing.Font("Montserrat ExtraBold", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_command.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbl_command.Location = new System.Drawing.Point(433, 412);
+            this.lbl_command.Name = "lbl_command";
+            this.lbl_command.Size = new System.Drawing.Size(46, 100);
+            this.lbl_command.TabIndex = 1;
+            this.lbl_command.Text = "0";
+            this.lbl_command.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(236, 68);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(235, 32);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(217)))), ((int)(((byte)(175)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(960, 520);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.ControlBox = false;
+            this.Controls.Add(this.lbl_command);
+            this.Controls.Add(this.lbl_timer);
+            this.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private Label lbl_timer;
+        private Label lbl_command;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,87 @@
+using Sidekick.WinForms;
+
 namespace blink
 {
     public partial class Form1 : Form
     {
+        private int _countdownDuration = 60;
+        private int _sleepDuration = 60 * 20;
+        private int _countdown;
+
         public Form1()
         {
             InitializeComponent();
+            ApiHelper.InitializeApiClient();
+            InitializeForm();
+        }
+
+        #region Logic
+        private async Task InitializeForm()
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            ShowIcon = true;
+            WindowState = FormWindowState.Minimized;
+            Bounds = Screen.PrimaryScreen.Bounds;
+            LocationExtension.SnapElementToMidScreen(this, lbl_timer);
+            lbl_command.Size = new Size(Size.Width, lbl_command.Size.Height);
+            LocationExtension.SnapElementHorizontalMid(this, lbl_command, Size.Height - 200);
+            await Arise();
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void ResetTimer()
+        {
+            _countdown = _countdownDuration;
+            lbl_timer.Text = _countdown.ToString();
+        }
+
+        public void Vanish()
+        {
+            timer.Stop();
+            Hide();
+            Thread.Sleep(1000 * _sleepDuration);
+            Arise();
+        }
+
+        public async Task Arise()
+        {
+            ResetTimer();
+
+            await UpdateColors();
+            UpdateCommand();
+
+            timer.Start();
+            Show();
+        }
+
+        private void UpdateCommand()
+        {
+            string command = CommandManager.GetRandomCommand();
+
+            lbl_command.Text = command;
+        }
+
+        private async Task UpdateColors()
+        {
+            string[] colors = await ColorApi.GetRandomColors();
+            BackColor = ColorTranslator.FromHtml($"#{colors[0]}");
+            lbl_command.ForeColor = ColorTranslator.FromHtml($"#{colors[1]}");
+            lbl_timer.ForeColor = ColorTranslator.FromHtml($"#{colors[1]}");
+        }
+        #endregion
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            _countdown--;
+            lbl_timer.Text = _countdown.ToString();
+
+            if (_countdown <= 0)
+                Vanish();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
