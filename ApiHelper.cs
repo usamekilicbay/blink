c74122e755e78bf644f2d4d36066f6e339468824
Usame Kilicbay <usame.kilicbay@gmail.com>
2022-04-07 13:09:23 +0300

    feat: create ApiHelper

---
+++ ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blink
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; private set; }
+
+        public static void InitializeApiClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
